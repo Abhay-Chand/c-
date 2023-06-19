fa9c4b2519d7cd0ecb335ab99d0d7da44caaa23b
Abhay-Chand <113808591+Abhay-Chand@users.noreply.github.com>
2023-06-19 06:36:17 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cin>>marks;
+    if(marks>33){
+        cout<<"pass"<<endl;
+    }
+}
---
+++ ifelse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int value;
+    cin>>value;
+    if(value %2==0){
+        cout<<"even";
+
+    }
+    else{
+        cout<<"odd";
+    }
+    return 0;
+}
