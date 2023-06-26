f92394a508e58cd27e6c76ab27104b67e6a3a1ad
Abhay-Chand <113808591+Abhay-Chand@users.noreply.github.com>
2023-06-26 05:58:36 +0530

    Add files via upload

---
+++ 1rectanglepattern.cpp
@@ -0,0 +1,19 @@
+// pattern printing 
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m,i,j;
+    cin>>n;
+    cin>>m;
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=m;j++){
+            cout<<"*";
+        
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+
+}
