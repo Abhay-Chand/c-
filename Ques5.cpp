86b409c70ec3ba6e2ba89393db3dad2eae39ae2b
Abhay-Chand <113808591+Abhay-Chand@users.noreply.github.com>
2023-07-03 05:55:45 +0530

    Add files via upload

---
+++ Ques5.cpp
@@ -0,0 +1,16 @@
+//print the first n factorial numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int factorial =1;
+    for(int i=1;i<=n;i++){
+        factorial*=i;
+        cout<<factorial<<endl;
+
+    }
+    
+    return 0;
+
+}
---
+++ Ques6.cpp
@@ -0,0 +1,15 @@
+//given 2 numbers a and b .find a raise to the b 
+//LOGIC 
+//A=a*a*a*a*a*a*.....b times
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int sum=0
+    for(int i=0;i<=b;i++){
+        a*=a;
+        
+    }
+    return 0;
+}
