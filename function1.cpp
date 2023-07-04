c410d89ec3df85390188a692eac369f53c4ac978
Abhay-Chand <113808591+Abhay-Chand@users.noreply.github.com>
2023-07-04 07:41:21 +0530

    Add files via upload

---
+++ function1.cpp
@@ -0,0 +1,9 @@
+//code for creating and declaring function
+#include<iostream>
+using namespace std;
+void welcome(){
+    cout<<"welcome to my github account ;)";
+}
+int main(){
+    welcome();
+}
